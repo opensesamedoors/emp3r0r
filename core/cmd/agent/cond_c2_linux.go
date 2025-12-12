7d43afc64b642b72007a5610b0b296c1eb9a48dd
jm33-m0 <jm33@jm33.me>
2025-12-12 03:50:45 +0000

    fix: improve conditionalC2FailNotify to ensure it only acts when started by a stager

--- core/cmd/agent/cond_c2_linux.go
+++ core/cmd/agent/cond_c2_linux.go
@@ -11,5 +11,9 @@ import (
 // conditionalC2FailNotify tells the parent (stager/loader) to recycle us.
 func conditionalC2FailNotify() {
 	ppid := os.Getppid()
+	// If we are not started by a stager, do nothing
+	if ppid <= 1 {
+		return
+	}
 	_ = syscall.Kill(ppid, syscall.SIGTRAP)
 }
